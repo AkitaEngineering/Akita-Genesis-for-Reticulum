6a68c21134c8fcc7351b41be0dd43c471aaa1fe4
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-03-15 13:18:20 -0400

    Update tasks.py

--- akita_genesis/tasks.py
+++ akita_genesis/tasks.py
@@ -1,1 +1,15 @@
+# akita_genesis/tasks.py
+from reticulum import Destination, Packet
+import json
+import random
+import time
+from .config import TASK_TIMEOUT
+import heapq
 
+class TaskManager:
+    def __init__(self, core):
+        self.core = core
+        self.task_queue = {}
+        self.task_results = {}
+        self.task_timeouts = {}
+        self.task_destination = Destination(None, Destination.BROADCAST

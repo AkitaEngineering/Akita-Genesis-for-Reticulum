f44396cf447e72cf3d86e3e13b2428413c06bfdc
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-08 11:26:53 -0400

    Create __init__.py

---
+++ akita_genesis/modules/__init__.py
@@ -0,0 +1,13 @@
+# akita_genesis/modules/__init__.py
+# Initialization for the modules package.
+
+# This file makes Python treat the 'modules' directory as a sub-package.
+# You could optionally import specific classes or functions from the modules
+# here to make them available directly under the 'modules' namespace, e.g.:
+#
+# from .communication import CommunicationManager
+# from .tasks import TaskManager
+#
+# However, it's often cleaner to keep this file empty or minimal and
+# have other parts of the application import directly from the specific module, e.g.:
+# from akita_genesis.modules.communication import CommunicationManager
